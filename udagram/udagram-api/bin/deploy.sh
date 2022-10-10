6e283ff38ba9f4eed442388bf673d200b4e58035
ahmedibrahimkenawi <ahmedibrahimkenawi@gmail.com>
2022-10-10 23:34:09 +0200

    use eb setenv

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,11 +1,11 @@
 eb list --profile default 
 eb use udagram-api-dev --profile default 
 eb deploy --profile default
-eb setenv POSTGRES_DB=$POSTGRES_DB
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-eb setenv POSTGRES_HOST=$POSTGRES_HOST
-eb setenv DB_PORT=$DB_PORT
-eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv PORT=$PORT
-eb setenv URL=$URL
+eb setenv POSTGRES_DB=$POSTGRES_DB --profile default 
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME --profile default 
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD --profile default 
+eb setenv POSTGRES_HOST=$POSTGRES_HOST --profile default 
+eb setenv DB_PORT=$DB_PORT --profile default 
+eb setenv JWT_SECRET=$JWT_SECRET --profile default 
+eb setenv PORT=$PORT --profile default 
+eb setenv URL=$URL --profile default 
