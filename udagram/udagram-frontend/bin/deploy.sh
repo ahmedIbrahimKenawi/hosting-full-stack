8650867e44ea68875cd7f281c48fa70ef98b3a7c
ahmedIbrahimKenawi <35937041+ahmedIbrahimKenawi@users.noreply.github.com>
2022-10-10 08:25:43 +0200

    update s3 deploy command

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive ./www s3://s3-udagram/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://s3-udagram/
+aws s3 cp --recursive ./www s3://udagramstatics/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagramstatics/
