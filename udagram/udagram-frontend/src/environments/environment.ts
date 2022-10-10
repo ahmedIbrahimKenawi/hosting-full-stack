2eb71106a97452004122f1f3a3b06f89c07503f9
ahmedIbrahimKenawi <35937041+ahmedIbrahimKenawi@users.noreply.github.com>
2022-10-10 08:27:19 +0200

    update eb link

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api5-dev.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-eqvkaiy3.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
